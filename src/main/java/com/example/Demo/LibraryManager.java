126b4fb5ad24e6dd8c4f85d295852a3256b957ad
CompanyProfile <alanood94895@gmail.com>
2026-06-07 14:43:47 +0400

    Create new class name LibraryManager

---
+++ src/main/java/com/example/Demo/LibraryManager.java
@@ -0,0 +1,52 @@
+package com.example.Demo;
+
+import java.util.ArrayList;
+
+public class LibraryManager {
+    private ArrayList<LibraryMember> members;
+
+    public LibraryManager() {
+        members = new ArrayList<>();
+    }
+
+    public void initializeMembers() {
+        members.add(new LibraryMember("M101", "Ahmed", "Standard"));
+        members.add(new LibraryMember("M102", "Sara", "Premium"));
+        members.add(new LibraryMember("M103", "John", "Standard"));
+    }
+
+    public void addMember(LibraryMember member) {
+        members.add(member);
+    }
+
+    public void displayMembers() {
+        System.out.println("\nLibrary Members:");
+
+        if (members.isEmpty()) {
+            System.out.println("No members available.");
+            return;
+        }
+
+        for (LibraryMember member : members) {
+            System.out.println(
+                    member.getMemberId() + " -> "
+                            + member.getFullName() + " -> "
+                            + member.getMembershipType());
+        }
+    }
+
+    public boolean deleteMemberById(String memberId) {
+
+        for (int i = 0; i < members.size(); i++) {
+
+            if (members.get(i).getMemberId().equalsIgnoreCase(memberId)) {
+
+                members.remove(i);
+                return true;
+            }
+        }
+
+        return false;
+    }
+}
+
