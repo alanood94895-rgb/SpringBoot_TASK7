675f5a1f5cd57314c12301ac37548f892edda334
CompanyProfile <alanood94895@gmail.com>
2026-06-07 15:08:20 +0400

    Create new class name EventRegistration

---
+++ src/main/java/com/example/Demo/EventRegistration.java
@@ -0,0 +1,19 @@
+package com.example.Demo;
+
+public class EventRegistration {
+    private int registrationId;
+    private String attendeeName;
+
+    public EventRegistration(int registrationId, String attendeeName) {
+        this.registrationId = registrationId;
+        this.attendeeName = attendeeName;
+    }
+
+    public int getRegistrationId() {
+        return registrationId;
+    }
+
+    public String getAttendeeName() {
+        return attendeeName;
+    }
+}
