e8c194723122cad8c6e47e2fd4f0d8a6663546c3
CompanyProfile <alanood94895@gmail.com>
2026-06-07 14:42:24 +0400

    Create new class name LibraryMember

---
+++ src/main/java/com/example/Demo/LibraryMember.java
@@ -0,0 +1,25 @@
+package com.example.Demo;
+
+public class LibraryMember {
+    private String memberId;
+    private String fullName;
+    private String membershipType;
+
+    public LibraryMember(String memberId, String fullName, String membershipType) {
+        this.memberId = memberId;
+        this.fullName = fullName;
+        this.membershipType = membershipType;
+    }
+
+    public String getMemberId() {
+        return memberId;
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public String getMembershipType() {
+        return membershipType;
+    }
+}
