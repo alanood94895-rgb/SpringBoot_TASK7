18f4bd1399d72efeed83a13c904508e6beee0a3f
CompanyProfile <alanood94895@gmail.com>
2026-06-07 14:44:51 +0400

    Create new class name LibraryDemo

---
+++ src/main/java/com/example/Demo/LibraryDemo.java
@@ -0,0 +1,29 @@
+package com.example.Demo;
+
+public class LibraryDemo {
+    public static void main(String[] args) {
+
+        LibraryManager manager = new LibraryManager();
+
+        manager.initializeMembers();
+
+        System.out.println("Initial Members:");
+        manager.displayMembers();
+
+        String memberIdToDelete = "M102";
+
+        System.out.println("\nDeleting Member ID: " + memberIdToDelete);
+
+        boolean deleted = manager.deleteMemberById(memberIdToDelete);
+
+        if (deleted) {
+            System.out.println("Member found and deleted successfully.");
+        } else {
+            System.out.println("Member not found.");
+        }
+
+        System.out.println("\nUpdated Members List:");
+        manager.displayMembers();
+    }
+}
+
