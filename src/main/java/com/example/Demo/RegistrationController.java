d540b8ba6e4240b5b3a7a8fe4e67e6b1434408db
CompanyProfile <alanood94895@gmail.com>
2026-06-07 15:16:31 +0400

    create new class name RegistrationController

--- src/main/java/com/example/Demo/RegistrationController.java
+++ src/main/java/com/example/Demo/RegistrationController.java
@@ -1,7 +1,34 @@
 package com.example.Demo;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 public class RegistrationController {
+    @Autowired
+    private RegistrationService registrationService;
+
+    @DeleteMapping("/registrations")
+    public BulkDeleteResponse deleteRegistrations(
+            @RequestParam String ids) {
+
+        List<Integer> idList = Arrays.stream(ids.split(","))
+                .map(Integer::parseInt)
+                .collect(Collectors.toList());
+
+        return registrationService.deleteMultipleByIds(idList);
+    }
+
+    @GetMapping("/registrations")
+    public Object getRegistrations() {
+        return registrationService.getAllRegistrations();
+    }
+
 }
